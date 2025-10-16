dd0ead361e04a35516e00728af0a30c0ec615c63
RayhanKabir-12286157 <rayhankabir.rabbin@cqumail.com>
2025-10-16 17:04:58 +1100

    Update DB.java

--- ths-server/src/main/java/com/mycompany/ths/server/db/DB.java
+++ ths-server/src/main/java/com/mycompany/ths/server/db/DB.java
@@ -4,7 +4,7 @@ import java.sql.*;
 
 public final class DB {
   static final String URL = "jdbc:mysql://localhost:3306/ths_enhanced";
-  static final String USER = "admin"; // <- set yours
-  static final String PASS = "admin";
+  static final String USER = "ths"; // <- set yours
+  static final String PASS = "ths123";
   public static Connection get() throws SQLException { return DriverManager.getConnection(URL, USER, PASS); }
 }
